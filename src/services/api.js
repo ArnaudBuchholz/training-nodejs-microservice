71694c53118d8e8c1cd892353828da719c56544e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-15 10:21:08 -0400

    Use service bootstrap, pipe the requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,35 +1,41 @@
 require('../service')(async app => {
-  const axios = require('axios')
+  const http = require('http')
   const registry = require('../registry')
 
   function fwd (service, url) {
     return async function (req, res) {
       try {
-        const { host, port } = await registry.getLocationOf(service)
-        axios.get(`http://${host}:${port}/${url || req.url.substring(1)}`, {
-          headers: req.headers,
-          validateStatus: status => (status >= 200 && status < 300) || [301, 302, 303, 304, 401, 403, 404].includes(status)
-        })
-          .then(response => {
-            res.status(response.status)
-            res.set(response.headers)
-            res.send(response.data)
-          })
-          .catch(err => {
-            // TODO notify registry of failure
-            console.error(err.toString())
+        const { host, port } = registry.getLocationOf(service)
+        const serviceRequest = http.request({ port, host, method: req.method, path: url || req.url })
+        serviceRequest.on('response', serviceResponse => {
+          const { statusCode, headers } = serviceResponse
+          if ((statusCode >= 200 && statusCode < 300) || [301, 302, 303, 304, 401, 403, 404].includes(statusCode)) {
+            res.writeHead(statusCode, headers)
+            serviceResponse.pipe(res)
+          } else {
+            console.error(`Unexpected service status code : ${statusCode}`)
             res.sendStatus(500)
-          })
-      } catch (err) {
-        console.error(`Unable to fetch location of ${service}`, err.toString())
+          }
+        })
+        serviceRequest.on('error', err => {
+          console.error(`Unexpected error while forwarding to ${service}`, err.toString())
+          res.sendStatus(500)
+        })
+        req.pipe(serviceRequest)
+      } catch (reason) {
+        console.error(`Unable to fetch location of ${service}`, reason.toString())
         res.sendStatus(500)
       }
     }
   }
 
   // /api/test --> test
-  app.get('/api/test', fwd('test', 'test'))
+  app.get('/api/test', fwd('test', '/test'))
 
   // default --> www
   app.get(/.*/, fwd('www'))
+
+  return {
+    port: 8080
+  }
 })
