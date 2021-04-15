48e57eeac2848bf25e8b80f8cfc558524b3614a5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-15 06:51:32 -0400

    Adapt to bootstrap

--- src/services/www.js
+++ src/services/www.js
@@ -1,16 +1,8 @@
-const express = require('express')
-const morgan = require('morgan')
-const registry = require('../registry')
-const { join } = require('path')
+require('../service')(async app => {
 
-const app = express()
+  app.use(express.static(process.env.WWW_ROOT))
 
-app.use(morgan('tiny'))
-
-app.use(express.static(process.env.WWW_ROOT || join(__dirname, '../www')))
-
-const server = app.listen(0, () => {
-  const port = server.address().port
-  console.log(`www service listening on port ${port}`)
-  registry.register('www', port)
+  return {
+    name: 'www'
+  }
 })
