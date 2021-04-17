80f27de8551c7e7c309c14f863d02fabf0f8a69e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-04-17 00:48:50 -0400

    Tests load balancing

--- tests/mocha/unit/registry.test.js
+++ tests/mocha/unit/registry.test.js
@@ -37,18 +37,18 @@ describe('registry service', () => {
     it('returns 404 on unknwon service', () => fetchUnknownService())
   })
 
-  describe('registering a fake service', () => {
-    let testInstanceId
+  describe('registering a test service', () => {
+    let test1InstanceId
 
     before(async () => {
       const response = await axios.post('http://localhost:8081/register', { name: 'test', host: '192.168.0.1', port: 1234 })
       assert.strictEqual(response.status, 200)
       assert.strictEqual(typeof response.data, 'object')
       assert.strictEqual(response.data.heartbeat, 1000)
-      testInstanceId = response.data.id
+      test1InstanceId = response.data.id
     })
 
-    it('returns the list of services', () => servicesRegistered([{ id: testInstanceId }]))
+    it('returns the list of services', () => servicesRegistered([{ id: test1InstanceId }]))
 
     it('returns the service info', async () => {
       const response = await axios.get('http://localhost:8081/test')
@@ -60,8 +60,39 @@ describe('registry service', () => {
 
     it('returns 404 on unknwon service', () => fetchUnknownService())
 
+    describe('registering a second test service', () => {
+      let test2InstanceId
+
+      before(async () => {
+        const response = await axios.post('http://localhost:8081/register', { name: 'test', host: '192.168.0.2', port: 5678 })
+        assert.strictEqual(response.status, 200)
+        assert.strictEqual(typeof response.data, 'object')
+        assert.strictEqual(response.data.heartbeat, 1000)
+        test2InstanceId = response.data.id
+      })
+
+      it('returns the list of services', () => servicesRegistered([{ id: test1InstanceId }, { id: test2InstanceId }]))
+
+      it('load balances between the two services', async () => {
+        const response1 = await axios.get('http://localhost:8081/test')
+        const response2 = await axios.get('http://localhost:8081/test')
+        assert.notStrictEqual(response1.data.id, response2.data.id)
+        assert.strictEqual(response1.data.name, response2.data.name)
+        assert.notStrictEqual(response1.data.host, response2.data.host)
+        assert.notStrictEqual(response1.data.port, response2.data.port)
+      })
+
+      it('returns 404 on unknwon service', () => fetchUnknownService())
+
+      after(async () => {
+        const response = await axios.delete(`http://localhost:8081/${test2InstanceId}`)
+        assert.strictEqual(response.status, 204)
+        await servicesRegistered([{ id: test1InstanceId }])
+      })
+    })
+
     after(async () => {
-      const response = await axios.delete(`http://localhost:8081/${testInstanceId}`)
+      const response = await axios.delete(`http://localhost:8081/${test1InstanceId}`)
       assert.strictEqual(response.status, 204)
       await servicesRegistered()
     })
