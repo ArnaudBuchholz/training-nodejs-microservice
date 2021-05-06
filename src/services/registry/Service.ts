60f89e29e6f31fe3c869ed33b7f2fb5f11a9e361
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-06 13:33:07 -0400

    Supports copy of IService

--- src/services/registry/Service.ts
+++ src/services/registry/Service.ts
@@ -1,13 +1,17 @@
-import { IService } from './IService'
+import { isIService, IService } from './IService'
 
 export class Service implements IService {
   name = ''
   host = 'localhost'
   port = 0
 
-  constructor (service: Service) {
-    this.name = service.name
-    this.host = service.host
-    this.port = service.port
+  constructor (service: any) {
+    if (isIService(service)) {
+      this.name = service.name
+      this.host = service.host
+      this.port = service.port
+    } else {
+      throw new Error('Unsupported parameter')
+    }
   }
 }
