d88721563ae90b67ae5d1eee4492bbce1b5b81c3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-05-06 13:32:44 -0400

    Check if a given object matches the interface

--- src/services/registry/IService.ts
+++ src/services/registry/IService.ts
@@ -3,3 +3,21 @@ export interface IService {
   host: string,
   port: number
 }
+
+export function isIService (arg: any) : arg is IService {
+  const key = Object.keys(arg)
+  if (key.length !== 3) {
+    return false
+  }
+  const { name, host, port } = arg
+  if (typeof name !== 'string' || !name) {
+    return false
+  }
+  if (typeof host !== 'string' || !host) {
+    return false
+  }
+  if (typeof port !== 'number' || !port) {
+    return false
+  }
+  return true
+}
